c37e170e3f9e3772edf0766fe0f6c86abeb928e9
112752 <1127523185@qq.com>
2023-08-15 20:53:30 +0800

    歌单列表优化

--- src/views/playlist-detail/index.vue
+++ src/views/playlist-detail/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SongTable from '../../components/SongTable.vue'
 import Comment from '../../components/Comment.vue'
-const { $http, $utils, $utils2 } = getCurrentInstance()?.proxy!
+const { $http, $utils, $utils2, $COMMON } = getCurrentInstance()?.proxy!
 const route = useRoute()
 const id = computed(() => route.query.id)
 
@@ -22,14 +22,15 @@ const currentComponent = shallowRef<any>(SongTable)
 const stop_a = watchEffect(() => {
   if (!id.value) return
   const idVal = toValue(id) as string
-  const tastA = $http.playlistdetail({ id: idVal }).then(res => {
+  const tastA = $http.playlistdetail({ id: idVal }).then(async res => {
     playlist.value = $utils.formatList('playlist', [res.playlist], 'middle')[0]
-    tracks.value = $utils.formatList('songlist', res.playlist.tracks)
+    let ids = res.playlist.trackIds.map((item: any) => item.id).join(',')
+    tracks.value = await $http.songDetail({ ids }).then(res => $utils.formatList('songlist', res.songs))
   })
   $utils2.loading([tastA])
 })
+// $http.songDetail()
 onBeforeRouteLeave(stop_a)
-
 const handleTabsChange = (index: number) => {
   console.log('index', index)
   if (index === tagInfoEnum.歌曲列表.index) {
@@ -55,7 +56,8 @@ const handleTabsChange = (index: number) => {
 
       <template #info-line-3>
         <RouterLink :to="{ name: 'index', query: { name: playlist.artistName, id: playlist.artistId } }">
-          <el-avatar size="small" style="vertical-align: middle" :src="playlist.avatar"></el-avatar>
+          <el-avatar size="small" style="vertical-align: middle"
+            :src="playlist.avatar + $COMMON.IMG_SIZE_SEARCH_PARAMS.squar.small"></el-avatar>
           &nbsp;<span>{{ playlist.artistName }}</span>
         </RouterLink>
         <span>{{ $utils.formartDate(playlist.createTime) }}</span>
